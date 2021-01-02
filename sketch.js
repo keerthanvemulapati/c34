106ae12c1ae8e3436b6c3457d489dae74ef51ee0
keerthanvemulapati <68475308+keerthanvemulapati@users.noreply.github.com>
2021-01-02 18:50:51 +0000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,46 @@
+var ball;
+var position,database;
+
+function setup(){
+    createCanvas(500,500);
+    database = firebase.database();
+    var location = database.ref("ball/position");
+    location.on("value", readoperation, showerror);
+    ball = createSprite(250,250,10,10);
+    ball.shapeColor = "red";
+}
+
+function draw(){
+    background("white");
+    if(keyDown(LEFT_ARROW)){
+        writePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        writePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        writePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        writePosition(0,+1);
+    }
+    drawSprites();
+}
+
+function writePosition(x,y){
+    database.ref("ball/position").update({
+        x: ball.x + x,
+        y: ball.y +y,
+    });
+
+}
+
+function readoperation(data){
+    position = data.val();
+    ball.x = position.x;
+    ball.y = position.y;
+
+}
+function showerror(){
+console.log("error")
+}
